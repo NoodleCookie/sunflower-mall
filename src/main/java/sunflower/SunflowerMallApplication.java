6720c8a64de3741130b801245e3029d2813d0e40
Izumi <1042631369@qq.com>
2021-10-29 10:14:36 +0800

    init

---
+++ src/main/java/sunflower/SunflowerMallApplication.java
@@ -0,0 +1,11 @@
+package sunflower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunflowerMallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SunflowerMallApplication.class, args);
+    }
+}
